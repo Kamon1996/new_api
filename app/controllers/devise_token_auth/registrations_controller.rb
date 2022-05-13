a158dcc8f9294e032f9216346c1eade7c27671b4
Dmitriy Kalganov <dmitriu355@gmail.com>
2022-05-13 12:12:11 +0300

    fixed bug with deleting posts with comments

--- app/controllers/devise_token_auth/registrations_controller.rb
+++ app/controllers/devise_token_auth/registrations_controller.rb
@@ -5,7 +5,7 @@ class DeviseTokenAuth::RegistrationsController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: { user: user.as_json }, status: :created
+      render json: user, status: :created
     else
       render json: { error: user.errors.full_messages }, status: :unprocessable_entity
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   
   validates :title, presence: true, length: {minimum:3, maximum:150}
   validates :body, presence: true, length: {minimum:3, maximum:500}
