2676f61d68a4087194c538874d4bb81396a4f2c9
Dmitriy Kalganov <dmitriu355@gmail.com>
2022-05-16 17:34:01 +0300

    rubocop -a

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,10 +10,8 @@
 #  updated_at :datetime         not null
 #
 class Comment < ApplicationRecord
-
   belongs_to :user
   belongs_to :post
 
-  validates :body, presence: true, length: {minimum: 3, maximum: 50}
-
+  validates :body, presence: true, length: { minimum: 3, maximum: 50 }
 end
